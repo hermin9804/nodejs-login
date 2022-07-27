dd88ec229c8756a898f36abd015b777365c92790
hermin9804 <hermin9804@gmail.com>
2022-07-28 00:56:32 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
@@ -12,6 +13,13 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
-app.use("/", home); //use -> 미들웨어 등록해주는 메서드
+
+app.use(bodyParser.json());
+
+// URL로 전달되는 한글, 공백 인코딩 처리
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//use -> 미들웨어 등록해주는 메서드
+app.use("/", home); 
 
 module.exports = app;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict"
 
-const home = (req, res) => {
-	res.render("home/index");
+const output = {
+	home: (req, res) => {
+		res.render("home/index");
+	},
+	login: (req, res) => {
+		res.render("home/login");
+	},
 };
 
-const login = (req, res) => {
-	res.render("home/login");
+const users = {
+	id: ["mher", "jhson", "kim"],
+	psword: ["1234", "12345", "123456"]
+}
+
+const process = {
+	login: (req, res) => {
+		const id = req.body.id,
+			psword = req.body.psword;
+
+		if (users.id.includes(id)) {
+			const idx = users.id.indexOf(id);
+			if (users.psword[idx] === psword) {
+				return (res.json({
+					success: true,
+					msg: "로그인 성공"
+				}));
+			}
+		}
+		return (res.json({
+			success: false,
+			msg: "로그인 실패"
+		}));
+	},
 };
 
 module.exports = {
-	home,
-	login,
+	output,
+	process,
 };
