d777efba566b6af0540ef58dfea1e3919795be69
hermin9804 <hermin9804@gmail.com>
2022-07-28 01:38:53 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,6 +18,16 @@ function login() {
 			"Content-Type": "application/json"
 		},
 		body: JSON.stringify(req),
-	}).then((res) => res.json())
-		.then((res) => console.log(res));
+	})
+		.then((res) => res.json())
+		.then((res) => {
+			if (res.success) {
+				location.href = "/";
+			} else {
+				alert(res.msg);
+			}
+		})
+		.catch((error) => {
+			console.log("로그인 중 에러 발생"); 
+		});
 }
