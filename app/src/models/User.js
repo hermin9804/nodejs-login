033b23b1f1c64ef00ac7781397574e446198fe86
hermin9804 <hermin9804@gmail.com>
2022-07-30 15:40:54 +0900

    User 모델 구현

---
+++ app/src/models/User.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const UserStorage = require("./UserStorage");
+
+class User {
+	constructor(body) {
+		this.body = body;
+	}
+
+	login() {
+		const body = this.body;
+		const { id, psword } = UserStorage.getUserInfo(body.id);
+
+		if (id) {
+			if (id === body.id && psword === body.psword) {
+				return ({success: true});
+			}
+			return ({success: false, msg: "비밀번호가 일치하지 않습니다."});
+		}
+		return ({success: false, msg: "아이디가 존재하지 않습니다."});
+	}
+}
+
+module.exports = User;
