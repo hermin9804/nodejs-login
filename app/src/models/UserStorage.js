6711ccceb6d338e806e28ca4b3b825716ad79535
hermin9804 <hermin9804@gmail.com>
2022-07-30 23:10:13 +0900

    fs모듈을 이용한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,40 +1,46 @@
 "use strict"
 
+const fs = require("fs").promises;
+
 class UserStorage {
-	static #users = {
-		id: ["mher", "jhson", "kim"],
-		psword: ["1234", "12345", "123456"],
-		name: ["허민", "주성", "김영재"]
-	};
-
-	static getUsers(...fields) {
-		const users = this.#users;
-		const newUsers = fields.reduce((newUsers, field) => {
-			if (users.hasOwnProperty(field)) {
-				newUsers[field] = users[field];
-			}
-			return (newUsers);
-		}, {});
-		return (newUsers);
-	}
-
-	static getUserInfo(id) {
-		const users = this.#users;
+	static #getUserInfo(data, id) {
+		const users = JSON.parse(data);
 		const idx = users.id.indexOf(id);
 		const usersKey = Object.keys(users);
 		const userInfo = usersKey.reduce((newUsers, info) => {
 			newUsers[info] = users[info][idx];
-			return (newUsers);
+			return newUsers;
 		}, {});
-		return (userInfo);
+
+		return userInfo;
+	}
+
+	static getUsers(...fields) {
+		//const users = this.#users;
+		const newUsers = fields.reduce((newUsers, field) => {
+			if (users.hasOwnProperty(field)) {
+				newUsers[field] = users[field];
+			}
+			return newUsers;
+		}, {});
+		return newUsers;
+	}
+
+	static getUserInfo(id) {
+		return fs
+			.readFile("./src/databases/users.json")
+			.then((data) => {
+				return (this.#getUserInfo(data, id));
+			})
+			.catch(console.error);
 	}
 
 	static save(userInfo) {
-		const users = this.#users;
+		//const users = this.#users;
 		users.id.push(userInfo.id);
 		users.psword.push(userInfo.psword);
 		users.name.push(userInfo.name);
-		return ({ success: true });
+		return { success: true };
 	}
 }
 
