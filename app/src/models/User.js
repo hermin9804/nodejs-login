6df55c56f4103ec3d08d02ef229b886b3c7f1e95
hermin9804 <hermin9804@gmail.com>
2022-08-01 18:53:42 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -13,17 +13,21 @@ class User {
 
 		if (id) {
 			if (id === client.id && psword === client.psword) {
-				return ({success: true});
+				return {success: true};
 			}
-			return ({success: false, msg: "비밀번호가 일치하지 않습니다."});
+			return {success: false, msg: "비밀번호가 일치하지 않습니다."};
 		}
-		return ({success: false, msg: "아이디가 존재하지 않습니다."});
+		return {success: false, msg: "아이디가 존재하지 않습니다."};
 	}
 
-	register() {
+	async register() {
 		const client = this.body;
-		const response = UserStorage.save(client);
+		try {
+		const response = await UserStorage.save(client);
 		return (response);
+		} catch (err) {
+			return { success: false, msg: err };
+		}
 	}
 }
 
