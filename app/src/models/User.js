02527b8043cb99ae1062334d6abb0c94d79e3be1
hermin9804 <hermin9804@gmail.com>
2022-08-04 22:18:44 +0900

    AWS RDS와 연동 & donenv로 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,19 @@ class User {
 
 	async login() {
 		const client = this.body;
-		const { id, psword } = await UserStorage.getUserInfo(client.id);
+		try {
+			const { id, psword } = await UserStorage.getUserInfo(client.id);
 
-		if (id) {
-			if (id === client.id && psword === client.psword) {
-				return {success: true};
+			if (id) {
+				if (id === client.id && psword === client.psword) {
+					return {success: true};
+				}
+				return {success: false, msg: "비밀번호가 일치하지 않습니다."};
 			}
-			return {success: false, msg: "비밀번호가 일치하지 않습니다."};
+			return {success: false, msg: "아이디가 존재하지 않습니다."};
+		} catch (err) {
+			return {success: false, msg: err};
 		}
-		return {success: false, msg: "아이디가 존재하지 않습니다."};
 	}
 
 	async register() {
